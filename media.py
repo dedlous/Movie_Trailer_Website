4d8b2e4c194f52901b11c504259c797caed8a08f
dedlous <dedlous@126.com>
2018-01-03 15:38:07 +0800

    reviewed advise

--- media.py
+++ media.py
@@ -2,13 +2,24 @@ import webbrowser
 # Movie class to store movie information
 class Movie():
     def __init__(self,title,storyline,poster_image_url,trailer_youku_url):
-        self.title=title
-        self.storyline=storyline
-        self.poster_image_url=poster_image_url
-        self.trailer_url=trailer_youku_url
+        '''
+        init Movie class with input parameter
 
-  # open webbrowser to show trailer
+        title               movie title
+        storyline           movie storyline
+        poster_image_url    movie poster image url
+        trailer_youku_url   movie traile on youku
+        '''
+        self.title = title
+        self.storyline = storyline
+        self.poster_image_url = poster_image_url
+        self.trailer_url = trailer_youku_url
+
+
     def show_trailer(self):
+        '''
+        open webbrowser to show trailer
+        '''
         webbrowser.open(self.trailer_url)
 
 
